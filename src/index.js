540a6265333725d8e5079e2ce6cf6f66bfa24e2e
asafperez02-cpu <asafperez02@gmail.com>
2026-03-30 20:11:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<React.StrictMode><App /></React.StrictMode>);
+// ביטול PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+// פונקציית רטט והתראות גלובלית
+window.testFeatures = function() {
+  if (navigator.vibrate) navigator.vibrate(200);
+  if ("Notification" in window) {
+    Notification.requestPermission().then(permission => {
+      if (permission === "granted") new Notification("הבדיקה הצליחה!");
+    });
+  }
+};
